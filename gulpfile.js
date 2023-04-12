2b5beb10b483cd4783ff3ff19d1de5e862b32786
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2023-04-11 20:18:55 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
 'use strict';
 
-var gulp = require('gulp');
-var buildTypes = require('./scripts/build-types');
+//const gulp = require('gulp');
+//var buildTypes = require('./scripts/build-types');
 
-gulp.task('build', function() {
-  return gulp
-    .src(['./src/types.js'])
-    .pipe(buildTypes)
-    .pipe(gulp.dest('./lib/'));
-});
+//gulp.task('build', function() {
+//  return gulp
+//    .src(['./src/types.js'])
+//    .pipe(buildTypes)
+//    .pipe(gulp.dest('./lib/'));
+//});
 
-gulp.task('default', ['build']);
+//gulp.task('default', ['build']);
+
+
+
+const { src, dest, series } = require('gulp');
+
+// The `build` function is exported so it is public and can be run with the `gulp` command.
+// It can also be used within the `series()` composition.
+function build(cb) {
+  // body omitted
+  src(['./src/types.js'])
+  .pipe(buildTypes)
+  .pipe(dest('./lib/'))
+  cb();
+}
+
+exports.build = build;
+exports.default = series(build);
